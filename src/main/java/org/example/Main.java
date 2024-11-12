c627825b33b53e36631a49571484bb3104f1caff
jason revill <jasonrevill10@gmail.com>
2024-11-12 07:37:05 +0000

    another branch 1 change

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,6 +8,7 @@ public class Main {
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.println("Hello and welcome!");
         System.out.println("This is branch 1");
+        System.out.println("This is another branch 1 change");
 
         for (int i = 1; i <= 5; i++) {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
