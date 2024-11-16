cdda8882395a6b9b5a17358e605c42747f979901
jason revill <jasonrevill10@gmail.com>
2024-11-16 17:55:43 +0000

    work on lambda

---
+++ src/main/java/com/revcom/CricketScorerLambda.java
@@ -0,0 +1,54 @@
+package com.revcom;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.LambdaLogger;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+
+
+import java.sql.SQLException;
+import java.time.Instant;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+public class CricketScorerLambda implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
+
+    public APIGatewayProxyResponseEvent handleRequest(final APIGatewayProxyRequestEvent input, final Context context) {
+        LambdaLogger logger = context.getLogger();
+
+        //  input.forEach((key,value)->logger.log(key+":"+value));
+
+        Map<String, String> responseHeaders = new HashMap<>();
+        responseHeaders.put("Content-Type", "application/json");
+        APIGatewayProxyResponseEvent response = new APIGatewayProxyResponseEvent().withHeaders(responseHeaders);
+
+        try {
+
+            CricketScorerDAO dao = new CricketScorerDAO();
+            CricketScorerDAO.Audit audit = new CricketScorerDAO.Audit(Instant.now(), "This has come from the lambda function");
+            dao.createAuditRow(audit);
+
+
+        } catch (SQLException e) {
+            logger.log(e.toString());
+            return response
+                    .withStatusCode(500)
+                    .withBody("Error in processing");
+        }
+
+        String output = "Hello world";
+        return response
+                .withStatusCode(200)
+                .withBody(output);
+
+        // return new APIResponse(event, 200, "Thanks for contacting scorer").returnJson();
+
+
+    }
+}
+
+
+
+
---
+++ src/main/java/com/revcom/SQSLambda.java
@@ -0,0 +1,30 @@
+package com.revcom;
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.SQSEvent;
+import com.amazonaws.services.lambda.runtime.events.SQSEvent.SQSMessage;
+public class SQSLambda implements RequestHandler<SQSEvent, Void> {
+
+    public Void handleRequest(SQSEvent sqsEvent, Context context) {
+        for (SQSMessage msg : sqsEvent.getRecords()) {
+            processMessage(msg, context);
+        }
+        context.getLogger().log("done");
+        return null;
+    }
+
+    private void processMessage(SQSMessage msg, Context context) {
+        try {
+            context.getLogger().log("Processed message " + msg.getBody());
+
+            // TODO: Do interesting work based on the new message
+
+        } catch (Exception e) {
+            context.getLogger().log("An error occurred");
+            throw e;
+        }
+
+    }
+}
+
+
