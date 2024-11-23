b0badb931b34496d6330dc20e9f2bc2df23deea6
jason revill <jasonrevill10@gmail.com>
2024-11-23 13:24:04 +0000

    more work

--- src/main/java/com/revcom/SQSLambda.java
+++ src/main/java/com/revcom/SQSLambda.java
@@ -1,25 +1,43 @@
 package com.revcom;
+
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.SQSEvent;
 import com.amazonaws.services.lambda.runtime.events.SQSEvent.SQSMessage;
+
+import java.sql.SQLException;
+import java.time.Instant;
+
 public class SQSLambda implements RequestHandler<SQSEvent, Void> {
 
     public Void handleRequest(SQSEvent sqsEvent, Context context) {
-        for (SQSMessage msg : sqsEvent.getRecords()) {
-            processMessage(msg, context);
+
+        CricketScorerDAO dao = new CricketScorerDAO();
+        try {
+            for (SQSMessage msg : sqsEvent.getRecords()) {
+                processMessage(msg, context, dao);
+
+            }
+            context.getLogger().log("done");
+            return null;
+        } catch (Exception e) {
+            context.getLogger().log(e.getMessage());
         }
-        context.getLogger().log("done");
+
         return null;
     }
 
-    private void processMessage(SQSMessage msg, Context context) {
+    private void processMessage(SQSMessage msg, Context context, CricketScorerDAO dao) throws Exception {
         try {
-            context.getLogger().log("Processed message " + msg.getBody());
+
+            String body = msg.getBody();
+
+            context.getLogger().log("Starting processing message " + body);
+            dao.createAuditRow(new CricketScorerDAO.Audit(Instant.now(), body));
 
             // TODO: Do interesting work based on the new message
 
-        } catch (Exception e) {
+        } catch (SQLException e) {
             context.getLogger().log("An error occurred");
             throw e;
         }
